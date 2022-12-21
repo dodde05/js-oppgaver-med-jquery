8e92aa0751abc3ca78c9c3d2467ba3618ff4bbce
dodde <dodde174@gmail.com>
2022-12-21 19:19:01 +0100

    oppgave complete

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-$(document).ready(function(){
+// For the tidiness' sake:
+// Whenever a variable has "total" in its name, it refers to a sum of numbers on the same row in the table
+// Whenever a variable has "sum" in its name, it refers to a sum of numbers on the same column in the table
 
-    const data = [
+$(document).ready(function(){ // JQuery method that waits until all HTML elements are loaded before running the function
+
+    //Making the data table
+
+    const data = [ // Entry format: [year, boys born, girls born]
         [2011, 31006, 29214],
         [2012, 30933, 29322],
         [2013, 30138, 28857],
@@ -27,7 +33,7 @@ $(document).ready(function(){
             row.append("<td>" + data[i][j] + "</td>");
         };
 
-        var genderTotal = data[i][1] + data[i][2];
+        var genderTotal = data[i][1] + data[i][2]; // Total number of births that year
         row.append("<td class='total'>" + genderTotal + "</td>");
 
         boySum += data[i][1];
@@ -40,12 +46,15 @@ $(document).ready(function(){
     $("#sumRow").append("<td>" + girlSum + "</td>");
     $("#sumRow").append("<td>" + totalSum + "</td>");
 
+
+    // Displaying the highest and lowest births
+
     var highest = {value: 0, year: 0};
-    var lowest = {value: data[0][1] + data[0][2], year: 0};
+    var lowest = {value: data[0][1] + data[0][2], year: 0}; // lowest starting value must be a value on the data array before the sorting in order to avoid it just returning 0 every time
     for(i=0; i<data.length; i++){
 
 
-        var genderTotal = data[i][1] + data[i][2];
+        var genderTotal = data[i][1] + data[i][2]; // Total number of births that year
         if(highest["value"] < genderTotal){
             highest["value"] = genderTotal;
             highest["year"] = data[i][0];
@@ -60,8 +69,21 @@ $(document).ready(function(){
     
     $("p").append("I " + highest["year"] + " var det flest fødsler med " + highest["value"] + ". I " + lowest["year"] + " var det færrest fødsler med " + lowest["value"] + ".");
 
+
+    // Making the checkboxes work and displaying the sum of the corresponding birth totals
+
+    var customSum = 0;
     $("input").click(function(){
-        
+
+        var genderTotal = Number(this.parentElement.parentElement.querySelector(".total").innerHTML); // Total number of births in the selected year
+        if(this.checked){
+            customSum += genderTotal;
+        } else{
+            customSum -= genderTotal;
+        }
+
+        $("#customSum").html(customSum);
+
     })
 
 });
